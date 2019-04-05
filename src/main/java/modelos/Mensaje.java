8cf6c718ffe777b615f986b8f43a0a9735a14786
Ansel Corona <AnselCorona@gmail.com>
2019-04-05 12:02:14 -0400

    Mensaje que se va a enviar.

---
+++ src/main/java/modelos/Mensaje.java
@@ -0,0 +1,54 @@
+package modelos;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Mensaje {
+
+    private int idDispositivo;
+    private Long temperatura;
+    private Long humedad;
+    private String fecha;
+
+    public Mensaje(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("DD/MM/YYYY HH:mm:ss");
+        Date date = new Date();
+
+        setIdDispositivo((int) (Math.random() * 500) + 1);
+        setFecha(simpleDateFormat.format(date));
+        setHumedad((long) (Math.random() * 100) + 1);
+        setTemperatura((long) (Math.random() * 50) + 1);
+    }
+
+    public int getIdDispositivo() {
+        return idDispositivo;
+    }
+
+    public void setIdDispositivo(int idDispositivo) {
+        this.idDispositivo = idDispositivo;
+    }
+
+    public Long getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(Long temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public Long getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(Long humedad) {
+        this.humedad = humedad;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
